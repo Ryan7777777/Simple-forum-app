f98dc3541e92f204b3bdb52276742b275944c00a
Ryan7777777 <ryanleung7777@gmail.com>
2019-11-22 17:32:20 +1300

    register page

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import App from './page/App.vue';
 import VueRouter from "vue-router";
-import Home from './page/AllPost.vue';
-import Register from './page/Register.vue';
+import Home from './page/AllPost';
+import Register from './page/Register';
 import VueRecaptcha from 'vue-recaptcha'
+import Message from "./page/Message";
 Vue.use(VueRouter);
 Vue.use(VueRecaptcha);
 const routes = [{
@@ -13,6 +14,10 @@ const routes = [{
   {
     path: "/newuser",
     component: Register
+  },
+  {
+    path:"/message",
+    component: Message
   }
 ];
 const router = new VueRouter ({
--- src/page/Register.vue
+++ src/page/Register.vue
@@ -1,10 +1,23 @@
 <template>
     <div>
       <div class="div1">
-        <a class="sign_up">Sign up a new account</a>
-        <a class="closebtn"  @click="page_close">&times;</a>
-        <a class="user_name"> User Name</a>
-        <input class="user_name_input">
+        <h1>Sign up a new account</h1>
+        <div class="icon">
+        <i class='fa fa-user-plus'></i>
+        </div>
+        <a class="closebtn"  @click="page_close">&times;</a><br>
+        <b class="user_name"> User Name</b>
+        <input class="user_name_input" type="text" placeholder="Username1902">
+        <b class="user_email" >User Email</b>
+        <input class="user_email_input" type="email" placeholder="1234@example.com">
+        <b class="first_name">First Name</b>
+        <b class="last_name">Last Name</b>
+        <input class="user_first_name_input" placeholder="Jason">
+        <input class="user_last_name_input" placeholder="Simth">
+        <b class="user_password">User Passeord</b>
+        <input class="user_passowrd_input" type="password" placeholder="Password">
+        <input class="user_password_input_confirm" type="password" placeholder="Confirmed Password">
+        <button class="register_btn">Register</button>
       </div>
     </div>
 </template>
@@ -25,7 +38,7 @@
   }
 </script>
 <style>
-  @import '../css/registerstyle.css';
   @import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css';
   @import 'https://fonts.googleapis.com/icon?family=Material+Icons';
+  @import '../css/registerstyle.css';
 </style>
