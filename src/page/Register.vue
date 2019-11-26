66085b72ebfb8c99a31cdff57f08f0fcc9947e78
Ryan7777777 <ryanleung7777@gmail.com>
2019-11-27 01:04:33 +1300

    implement bootstrap form

--- src/page/Register.vue
+++ src/page/Register.vue
@@ -5,18 +5,32 @@
         <div class="icon">
         <i class='fa fa-user-plus'></i>
         </div>
-        <a class="closebtn"  @click="page_close">&times;</a><br>
-        <b class="user_name"> User Name</b>
-        <input class="user_name_input" type="text" placeholder="Username1902">
-        <b class="user_email" >User Email</b>
-        <input class="user_email_input" type="email" placeholder="1234@example.com">
-        <b class="first_name">First Name</b>
-        <b class="last_name">Last Name</b>
-        <input class="user_first_name_input" placeholder="Jason">
-        <input class="user_last_name_input" placeholder="Simth">
-        <b class="user_password">User Passeord</b>
-        <input class="user_passowrd_input" type="password" placeholder="Password">
-        <input class="user_password_input_confirm" type="password" placeholder="Confirmed Password">
+        <a class="closebtn"  @click="page_close">&times;</a>
+        <form>
+          <div class="form-group">
+            <label for="user_name_input"> User Name</label>
+            <input class="form-control" id ="user_name_input" type="text" placeholder="Username1902">
+          </div>
+          <div class="form-group">
+            <label for="user_email_input" >User Email</label>
+            <input class="form-control" id = "user_email_input" type="email" placeholder="1234@example.com">
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for ="first_name_input">First Name</label>
+              <input class = "form-control" id="first_name_input" placeholder="Jason">
+            </div>
+            <div class="form-group col-md-6">
+              <label for ="last_name_input">Last Name</label>
+              <input class = "form-control" id ="last_name_input" placeholder="Simth">
+            </div>
+          </div>
+          <div class="form-group">
+              <label for="user_passowrd_input">User Password</label>
+               <input class = "form-control" id="user_passowrd_input" type="password" placeholder="Password">
+              <input class = "form-control" id ="user_password_input_confirm" type="password" placeholder="Confirmed Password">
+          </div>
+        </form>
         <button class="register_btn">Register</button>
       </div>
     </div>
@@ -41,4 +55,5 @@
   @import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css';
   @import 'https://fonts.googleapis.com/icon?family=Material+Icons';
   @import '../css/registerstyle.css';
+  @import 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css';
 </style>
